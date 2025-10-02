2c16610eb10e77e7519cd899e381cf145e219faa
Baontin <tinvtpd07947@gmail.com>
2025-10-02 11:28:17 +0700

    feat: add recruiter profile controller and service

---
+++ src/main/java/com/baontin/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package com.baontin.jobportal.services;
+
+import com.baontin.jobportal.entity.RecruiterProfile;
+import com.baontin.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+}
